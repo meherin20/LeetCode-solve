b1d5db434f623e0ce0d0e872e1052e87845f1b73
Meherin Sohani <msohani2330420@bscse.uiu.ac.bd>
2026-01-20 23:29:00 +0600

    Time: 10 ms (24.98%) | Memory: 9.7 MB (81.12%) - LeetSync

---
+++ 686-repeated-string-match/repeated-string-match.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int repeatedStringMatch(string a, string b) {
+        string repeated = "";
+        int count = 0;
+
+      
+        while (repeated.length() < b.length()) {
+            repeated += a;
+            count++;
+        }
+
+       
+        if (repeated.find(b) != string::npos)
+            return count;
+
+       
+        repeated += a;
+        count++;
+
+        if (repeated.find(b) != string::npos)
+            return count;
+
+        return -1;
+    }
+};
