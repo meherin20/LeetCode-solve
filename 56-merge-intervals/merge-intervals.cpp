044b23fb37fb6c491ae4dd0b1420c8514713afec
Meherin Sohani <msohani2330420@bscse.uiu.ac.bd>
2026-02-04 04:52:12 +0600

    Time: 3 ms (89.28%) | Memory: 23.8 MB (80.49%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if (intervals.empty()) return {};
+
+        
+        sort(intervals.begin(), intervals.end());
+
+        vector<vector<int>> merged;
+        merged.push_back(intervals[0]);
+
+        for (int i = 1; i < intervals.size(); i++) {
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+
+           
+            if (start <= merged.back()[1]) {
+                merged.back()[1] = max(merged.back()[1], end);
+            } else {
+                merged.push_back(intervals[i]);
+            }
+        }
+
+        return merged;
+    }
+};
